079c3cd58cb56e325208effcbe95e51dd341b34f
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-02-12 10:39:17 +0530

    added a valid-watcher component

---
+++ components/form-controls/valid-watcher.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script>
+export default {
+	props: ['valid'],
+	watch: {
+		valid: {
+			immediate: true,
+			handler () {
+				this.$emit('change', this.valid)
+			}
+		}
+	}
+};
+</script>
+
+<style></style>
