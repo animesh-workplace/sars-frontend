9f463c35d1ec503a68c7a55c600f05dcdcdf3a78
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-02-13 10:41:31 +0530

    added tag

---
+++ components/upload/tag.vue
@@ -0,0 +1,24 @@
+<template>
+	<span :class="tagtype ? 'tag is-success' : 'tag is-danger'">
+		<svg class="icon is-small has-fill-white">
+			<use xlink:href="@/assets/images/icons/bds.svg#check-bold-g" v-if="tagtype"></use>
+			<use xlink:href="@/assets/images/icons/bds.svg#cross-bold-g" v-else></use>
+		</svg>&nbsp;&nbsp;{{ tagtype ? 'Passed' : 'Failed' }}
+	</span>
+</template>
+
+<script>
+export default {
+	props: {
+		name: {
+			type: String
+		},
+		tagtype: {
+			type: Boolean,
+			required: true
+		}
+	}
+};
+</script>
+
+<style scoped></style>
