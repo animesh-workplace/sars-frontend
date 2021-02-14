0df4f90569f765b7ec36657f67b961d20a85b849
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-02-14 10:42:30 +0530

    added normal layout

---
+++ layouts/normal.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<Navbar/>
+		<Nuxt/>
+	</div>
+</template>
+
+<script>
+import Navbar from '@/components/navbar/top-nav.vue'
+
+export default {
+	name: 'normal-layout',
+	middleware: 'auth',
+	data: () => ({
+	}),
+	components: {
+		Navbar
+	},
+	computed: {
+	},
+	beforeMount(){
+		// if(this.$auth.loggedIn) {
+		// 	this.$store.dispatch('user-info-store/set_userinfo')
+		// }
+	},
+};
+</script>
+
+<style scoped></style>
