2b85a7112e7d7440144f435a5e857f6ecb29c867
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-02-14 10:42:03 +0530

    added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<Nuxt/>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'default-layout',
+	middleware: 'guest',
+	data: () => ({
+	}),
+	components: {
+	},
+	computed: {
+	},
+	beforeMount(){
+	},
+};
+</script>
+
+<style scoped></style>
