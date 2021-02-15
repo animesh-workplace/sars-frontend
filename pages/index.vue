0f174c24f444f3682929430729e77e1c77dccbd3
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-02-15 10:44:45 +0530

    added index page

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<section class="hero is-fullheight">
+		<div class="hero-body">
+			<div class="container">
+				<div class="column is-8 is-offset-2">
+					<div class="box is-floating">
+						WIP
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+import { map, forEach } from "lodash"
+import { mapFields } from 'vuex-map-fields'
+
+export default {
+	layout: 'normal',
+	middleware: 'redirect',
+	data: () => ({
+	}),
+	components: {
+	},
+	computed: {
+		...mapFields([
+			'active'
+		]),
+	},
+	methods: {
+	},
+	beforeMount() {
+		this.active = 'Home'
+	},
+	mounted() {
+		this.$nextTick(()=>{
+		})
+	}
+};
+</script>
+
+<style scoped></style>
