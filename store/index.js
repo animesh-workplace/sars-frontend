ae59498834368cdd022367719c31edb1095d4189
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-02-27 12:07:39 +0530

    added timeout to store

--- store/index.js
+++ store/index.js
@@ -3,16 +3,26 @@ import { ToastProgrammatic as Toast } from 'buefy';
 import { getField, updateField } from 'vuex-map-fields'
 
 export const state = () => ({
-	active: 'Home'
+	active: 'Home',
+	timeout: null,
 });
 
 export const getters = {
 	getField,
+	get_timeout(state) {
+		return state.timeout
+	}
 };
 
 export const mutations = {
 	updateField,
+	SET_TIMEOUT(state, payload) {
+		state.timeout = payload
+	}
 }
 
 export const actions = {
+	async set_timeout({ commit }, payload) {
+		commit('SET_TIMEOUT', payload)
+	}
 }
