0aa35654f89e8ef592633ca8ed9a7aa674a7673e
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-03-18 14:44:02 +0530

    updated pie-chart to include count

--- components/charts/pie-chart-stats.vue
+++ components/charts/pie-chart-stats.vue
@@ -1,13 +1,16 @@
 <template>
-	<div v-if="chartdata">
+	<div v-if="chartdata" class="set-relative">
 		<pie-chart
 			:donut="true"
 			:legend="true"
 			:colors="colors"
 			:data="chartdata"
 			class="pie-chart"
+			:library="options"
+			:plugins="options.plugins"
 		>
 		</pie-chart>
+		<span class="tag shift-up is-unselectable">Total: {{ total_sum }}</span>
 	</div>
 </template>
 
@@ -17,7 +20,13 @@ import { forEach } from 'lodash'
 export default {
 	data: () => ({
 		chartdata: null,
-		colors: [ '#087FD2', '#E26EA5', '#F45564', '#FF8F41', '#FFC748', '#00C46C', '#50B98C', '#00C8B5', '#01BFEA', '#AA50B5' ]
+		colors: [ '#087FD2', '#E26EA5', '#F45564', '#FF8F41', '#FFC748', '#00C46C', '#50B98C', '#00C8B5', '#01BFEA', '#AA50B5' ],
+		total_sum: 0,
+		options: {
+			animation: {
+				animateRotate: true
+			},
+		}
 	}),
 	components: {
 	},
@@ -30,10 +39,13 @@ export default {
 			const data = await this.$axios.$post('/files/metadata-stats/')
 			if(data.message == null) {
 				let temp = {}
+				let sum = 0
 				forEach(data, (d,i)=> temp[i.split('Insacog_')[1]] = d)
+				forEach(data, (d,i)=> sum = sum + d)
+				this.total_sum = sum
 				this.chartdata = temp
 			}
-		}
+		},
 	},
 	mounted() {
 		this.$nextTick(()=>{
@@ -43,4 +55,13 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.set-relative {
+	position: relative;
+}
+.shift-up {
+	position: absolute;
+	top: 51%;
+	left: 42.5%;
+}
+</style>
