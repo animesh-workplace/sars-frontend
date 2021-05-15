57e34bf8dc49f2dbc958fdce0fda685b5d48ef99
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-05-15 19:54:00 +0530

    removed pagination from this place and modified the look of loader

--- pages/my_data.vue
+++ pages/my_data.vue
@@ -1,21 +1,17 @@
 <template>
 	<section class="section mt-6">
-		<div class="column">
+<!-- 		<div class="column">
 			<BarChartStats/>
-		</div>
+		</div> -->
 
 <!-- 		<div class="column">
 			<D3Chart/>
 		</div> -->
 
-		<div class="box is-raised table-container is-unselectable">
-			<div v-if="enable_table">
-				<Table :tabledata="$vs.getPage(metadata, page, 30)"/>
-				<div class="has-text-centered">
-					<vs-pagination color="#D5CCBF" :dotted-number="10" v-model="page" :length="$vs.getLength(metadata, 30)" />
-				</div>
-			</div>
-			<div v-else>
+		<div class="box is-raised is-unselectable">
+			<div class="box medium has-skeleton" v-if="table_loading"></div>
+			<Table :tabledata="metadata" v-if="!table_loading && enable_table"/>
+			<div v-if="!enable_table && !table_loading">
 				<span class="subtitle is-5 has-text-grey-dark">No data uploaded yet</span>
 			</div>
 		</div>
@@ -25,7 +21,7 @@
 <script>
 import { map, forEach } from "lodash"
 import { mapFields } from 'vuex-map-fields'
-import Table from "@/components/table/table.vue"
+import Table from "@/components/table/table-advanced.vue"
 import D3Chart from "@/components/charts/d3-chart.vue"
 import BarChartStats from "@/components/charts/bar-chart-stats.vue"
 
@@ -34,7 +30,8 @@ export default {
 	middleware: ['auth', 'auth_logout'],
 	data: () => ({
 		page: 1,
-		metadata: null
+		metadata: null,
+		table_loading: true,
 	}),
 	components: {
 		Table,
@@ -63,13 +60,18 @@ export default {
 		this.active = 'Sequences'
 	},
 	mounted() {
-		const loading = this.$vs.loading()
-		this.$nextTick(()=>{
-			this.get_metadata_table()
-			loading.close()
+		// const loading = this.$vs.loading()
+		this.$nextTick(async ()=>{
+			await this.get_metadata_table()
+			// loading.close()
+			this.table_loading = false
 		})
 	}
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.medium {
+	height: 770px;
+}
+</style>
