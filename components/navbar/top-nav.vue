9331262b3b187067105a70b35c11f80c3a96dbd3
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-06-15 13:04:00 +0530

    enabled the download button

--- components/navbar/top-nav.vue
+++ components/navbar/top-nav.vue
@@ -38,8 +38,8 @@
 		<template #right>
 			<client-only>
 				<div>
-					<div class="button is-success mr-2" disabled>
-						<span>Download [Under-development]</span>
+					<div class="button is-success mr-2" @click="download">
+						<span>Download</span>
 					</div>
 				</div>
 				<div>
