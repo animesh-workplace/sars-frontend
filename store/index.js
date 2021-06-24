794248200a3b30572d6a36ee0ba1b0f944aca5f4
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-06-24 21:22:58 +0530

    created store creation of websocket

--- store/index.js
+++ store/index.js
@@ -6,6 +6,7 @@ export const state = () => ({
 	active: 'Home',
 	timeout: null,
 	missing_columns: [],
+	websocket_connection: null,
 });
 
 export const getters = {
@@ -19,11 +20,20 @@ export const mutations = {
 	updateField,
 	SET_TIMEOUT(state, payload) {
 		state.timeout = payload
+	},
+	SET_WEBSOCKET_CONNECTION(state, payload) {
+		state.websocket_connection = payload
+		state.websocket_connection.onmessage = (event) => {
+			let websocket_data = JSON.parse(event.data)['message']
+		}
 	}
 }
 
 export const actions = {
 	async set_timeout({ commit }, payload) {
 		commit('SET_TIMEOUT', payload)
+	},
+	async set_websocket_connection({ commit }) {
+		commit('SET_WEBSOCKET_CONNECTION', new WebSocket(`${process.env.WS_BASE_URL}/frontend/`))
 	}
 }
