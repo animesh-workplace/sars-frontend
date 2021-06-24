537b5849831ffaaa8f2a30ef4f0de5b6301dd213
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-06-24 23:37:53 +0530

    added a new component cool tag for clade/lineage/scorpio/substitution/deletion

---
+++ components/table/cool-tag.vue
@@ -0,0 +1,26 @@
+<template>
+	<vs-button size="small" transparent active :color="theme" class="no-pointer-event m-0">
+		<span class="has-text-weight-semibold is-uppercase has-text-black">
+			{{ name }}
+		</span>
+	</vs-button>
+</template>
+
+<script>
+export default {
+	props: {
+		theme: {
+			type: String,
+		},
+		name: {
+			type: String,
+		},
+	}
+};
+</script>
+
+<style scoped>
+	.no-pointer-event {
+		pointer-events: none;
+	}
+</style>
