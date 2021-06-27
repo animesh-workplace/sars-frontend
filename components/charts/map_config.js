f07b6051dc49e6e60d1cebf724ce2014a786377d
Animesh_Singh <animesh.workplace@gmail.com>
2021-06-27 16:42:47 +0530

    added map_config

---
+++ components/charts/map_config.js
@@ -0,0 +1,187 @@
+export const MAP_META = {
+  "Andhra Pradesh": {
+  	"code": "AP",
+	"geoDataFile": "@/components/charts/maps/andhrapradesh.json",
+	"mapType": "STATE",
+  },
+  "Arunachal Pradesh": {
+  	"code": "AR",
+	"geoDataFile": "@/components/charts/maps/arunachalpradesh.json",
+	"mapType": "STATE",
+  },
+  "Assam": {
+  	"code": "AS",
+	"geoDataFile": "@/components/charts/maps/assam.json",
+	"mapType": "STATE",
+  },
+  "Bihar": {
+  	"code": "BR",
+	"geoDataFile": "@/components/charts/maps/bihar.json",
+	"mapType": "STATE",
+  },
+  "Chhattisgarh": {
+  	"code": "CT",
+	"geoDataFile": "@/components/charts/maps/chhattisgarh.json",
+	"mapType": "STATE",
+  },
+  "Goa": {
+  	"code": "GA",
+	"geoDataFile": "@/components/charts/maps/goa.json",
+	"mapType": "STATE",
+  },
+  "Gujarat": {
+  	"code": "GJ",
+	"geoDataFile": "@/components/charts/maps/gujarat.json",
+	"mapType": "STATE",
+  },
+  "Haryana": {
+  	"code": "HR",
+	"geoDataFile": "@/components/charts/maps/haryana.json",
+	"mapType": "STATE",
+  },
+  "Himachal Pradesh": {
+  	"code": "HP",
+	"geoDataFile": "@/components/charts/maps/himachalpradesh.json",
+	"mapType": "STATE",
+  },
+  "Jammu and Kashmir": {
+  	"code": "JK",
+	"geoDataFile": "@/components/charts/maps/jammukashmir.json",
+	"mapType": "STATE",
+  },
+  "Jharkhand": {
+  	"code": "JH",
+	"geoDataFile": "@/components/charts/maps/jharkhand.json",
+	"mapType": "STATE",
+  },
+  "Karnataka": {
+  	"code": "KA",
+	"geoDataFile": "@/components/charts/maps/karnataka.json",
+	"mapType": "STATE",
+  },
+  "Kerala": {
+  	"code": "KL",
+	"geoDataFile": "@/components/charts/maps/kerala.json",
+	"mapType": "STATE",
+  },
+  "Madhya Pradesh": {
+  	"code": "MP",
+	"geoDataFile": "@/components/charts/maps/madhyapradesh.json",
+	"mapType": "STATE",
+  },
+  "Maharashtra": {
+  	"code": "MH",
+	"geoDataFile": "@/components/charts/maps/maharashtra.json",
+	"mapType": "STATE",
+  },
+  "Manipur": {
+  	"code": "MN",
+	"geoDataFile": "@/components/charts/maps/manipur.json",
+	"mapType": "STATE",
+  },
+  "Meghalaya": {
+  	"code": "ML",
+	"geoDataFile": "@/components/charts/maps/meghalaya.json",
+	"mapType": "STATE",
+  },
+  "Mizoram": {
+  	"code": "MZ",
+	"geoDataFile": "@/components/charts/maps/mizoram.json",
+	"mapType": "STATE",
+  },
+  "Nagaland": {
+  	"code": "NL",
+	"geoDataFile": "@/components/charts/maps/nagaland.json",
+	"mapType": "STATE",
+  },
+  "Odisha": {
+  	"code": "OR",
+	"geoDataFile": "@/components/charts/maps/odisha.json",
+	"mapType": "STATE",
+  },
+  "Punjab": {
+  	"code": "PB",
+	"geoDataFile": "@/components/charts/maps/punjab.json",
+	"mapType": "STATE",
+  },
+  "Rajasthan": {
+  	"code": "RJ",
+	"geoDataFile": "@/components/charts/maps/rajasthan.json",
+	"mapType": "STATE",
+  },
+  "Sikkim": {
+  	"code": "SK",
+	"geoDataFile": "@/components/charts/maps/sikkim.json",
+	"mapType": "STATE",
+  },
+  "Tamil Nadu": {
+  	"code": "TN",
+	"geoDataFile": "@/components/charts/maps/tamilnadu.json",
+	"mapType": "STATE",
+  },
+  "Telangana": {
+  	"code": "TG",
+	"geoDataFile": "@/components/charts/maps/telangana.json",
+	"mapType": "STATE",
+  },
+  "Tripura": {
+  	"code": "TR",
+	"geoDataFile": "@/components/charts/maps/tripura.json",
+	"mapType": "STATE",
+  },
+  "Uttarakhand": {
+  	"code": "UT",
+	"geoDataFile": "@/components/charts/maps/uttarakhand.json",
+	"mapType": "STATE",
+  },
+  "Uttar Pradesh": {
+  	"code": "UP",
+	"geoDataFile": "@/components/charts/maps/uttarpradesh.json",
+	"mapType": "STATE",
+  },
+  "West Bengal": {
+  	"code": "WB",
+	"geoDataFile": "@/components/charts/maps/westbengal.json",
+	"mapType": "STATE",
+  },
+  "Andaman and Nicobar Islands": {
+  	"code": "AN",
+	"geoDataFile": "@/components/charts/maps/andamannicobarislands.json",
+	"mapType": "STATE",
+  },
+  "Chandigarh": {
+  	"code": "CH",
+	"geoDataFile": "@/components/charts/maps/chandigarh.json",
+	"mapType": "STATE",
+  },
+  "Dadra and Nagar Haveli and Daman and Diu": {
+  	"code": "DN",
+	"geoDataFile": "@/components/charts/maps/dnh-and-dd.json",
+	"mapType": "STATE",
+  },
+  "Delhi": {
+  	"code": "DL",
+	"geoDataFile": "@/components/charts/maps/delhi.json",
+	"mapType": "STATE",
+  },
+  "Ladakh": {
+  	"code": "LA",
+	"geoDataFile": "@/components/charts/maps/ladakh.json",
+	"mapType": "STATE",
+  },
+  "Lakshadweep": {
+  	"code": "LD",
+	"geoDataFile": "@/components/charts/maps/lakshadweep.json",
+	"mapType": "STATE",
+  },
+  "Puducherry": {
+  	"code": "PY",
+	"geoDataFile": "@/components/charts/maps/puducherry.json",
+	"mapType": "STATE",
+  },
+  "India": {
+  	"code": "IN",
+	"geoDataFile": "@/components/charts/maps/india.json",
+	"mapType": "COUNTRY",
+  }
+}
