6f13436bb6b06075f160139dfd6492f4225c2254
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-06-29 20:13:20 +0530

    added tags to tree-map and made it dynamic to the data available

--- components/charts/tree-map-chart.vue
+++ components/charts/tree-map-chart.vue
@@ -1,15 +1,27 @@
 <template>
 	<div>
+		<div class="tags mt-2">
+			<span
+				:key="gene"
+				class="tag is-info is-clickable"
+				@click="change_treemap(gene)"
+				v-for="gene in ChartData.genes"
+			>
+				{{ gene }}
+			</span>
+		</div>
 		<v-chart
 			class="chart"
 			:loading="false"
 			:option="options"
 			:loading-options="loader_option"
+			ref="treemap-chart"
 		/>
 	</div>
 </template>
 
 <script>
+import { forEach, sortBy, map } from "lodash"
 import { use } from 'echarts/core'
 import {
 	TitleComponent,
@@ -22,12 +34,26 @@ import { CanvasRenderer } from 'echarts/renderers'
 use([
 	TreemapChart,
 	CanvasRenderer,
-	// TitleComponent,
+	TitleComponent,
 	TooltipComponent,
 ])
 
 export default {
 	data: () => ({
+		color_scheme: {
+			S: '#37a2da',
+			E: '#8378ea',
+			M: '#9d96f5',
+			N: '#e7bcf3',
+			ORF1a: '#e690d1',
+			ORF1b: '#e062ae',
+			ORF3a: '#fb7293',
+			ORF6: '#ff9f7f',
+			ORF7a: '#ffdb5c',
+			ORF7b: '#9fe6b8',
+			ORF8: '#67e0e3',
+			ORF9b: '#32c5e9',
+		},
 		state_name: null,
 		clicked: false,
 		loader_option: {
@@ -52,78 +78,34 @@ export default {
 			},
 			series: [
 				{
-					roam: false,
-					nodeClick: false,
+					// roam: false,
+					// nodeClick: false,
 					breadcrumb: {
-						show: false
+						// show: false,
+						borderRadius: 5,
+						itemStyle: {
+							textStyle: {
+								fontFamily: 'Averta',
+								fontWeight: 500
+							},
+						}
 					},
 					name: 'Mutations',
 					type: 'treemap',
 					visibleMin: 50,
 					stack: 'total',
 					label: {
-						show: true
+						show: true,
+						fontFamily: 'Averta',
+						fontWeight: 500,
+						label: '{b} ({c})'
+						// color: '#51434B'
 					},
 	                itemStyle: {
-	                    borderRadius: 10,
-	                    gapWidth: 2,
+	                    borderRadius: 5,
+	                    gapWidth: 1,
 	                },
-					data: [
-					    {
-					        "value": 40,
-					        "name": "Spike",
-					        "path": "Spike",
-					        "children": [
-					            {
-					                "value": 12,
-					                "name": "S:123",
-					                "path": "Spike/S:123"
-					            },
-					            {
-					                "value": 28,
-					                "name": "S:124",
-					                "path": "Spike/S:124"
-					            },
-					            {
-					                "value": 20,
-					                "name": "S:125",
-					                "path": "Spike/S:125"
-					            },
-					            {
-					                "value": 16,
-					                "name": "S:126",
-					                "path": "Spike/S:126"
-					            }
-					        ]
-					    },
-					    {
-					        "value": 50,
-					        "name": "Membrane",
-					        "path": "Membrane",
-					        "children": [
-					            {
-					                "value": 120,
-					                "name": "M:123",
-					                "path": "Membrane/M:123"
-					            },
-					            {
-					                "value": 25,
-					                "name": "M:124",
-					                "path": "Membrane/M:124"
-					            },
-					            {
-					                "value": 40,
-					                "name": "M:125",
-					                "path": "Membrane/M:125"
-					            },
-					            {
-					                "value": 56,
-					                "name": "M:126",
-					                "path": "Membrane/M:126"
-					            }
-					        ]
-					    }
-					]
+					data: []
 				},
 			]
 		}
@@ -135,12 +117,57 @@ export default {
 		[THEME_KEY]: "light"
 	},
 	props: {
+		ChartData: {
+			type: Object
+		},
+		State: {
+			type: Object
+		}
 	},
 	watch: {
+		ChartData(value) {
+			this.state_name = this.State.name
+			this.options.series[0].data.push({
+				name: value['India']['S'].name,
+				children: value['India']['S'].children,
+				value: Object.keys(value['India']['S'].children).length,
+			})
+		},
+		State(value) {
+			if(this.state_name != value.district) {
+				if(Object.keys(this.ChartData).length > 0) {
+					this.state_name = value.district
+					this.$refs['treemap-chart'].clear()
+					this.options.series[0].data = []
+					if(this.ChartData[value.district] != undefined) {
+						this.options.series[0].data.push({
+			                itemStyle: {
+								color: this.color_scheme['S'],
+			                },
+							name: this.ChartData[value.district]['S'].name,
+							children: this.ChartData[value.district]['S'].children,
+							value: Object.keys(this.ChartData[value.district]['S'].children).length,
+						})
+					}
+				}
+			}
+		}
 	},
 	computed: {
 	},
 	methods: {
+		change_treemap(gene) {
+			this.$refs['treemap-chart'].clear()
+			this.options.series[0].data = []
+			this.options.series[0].data.push({
+                itemStyle: {
+					color: this.color_scheme[gene],
+                },
+				name: this.ChartData['India'][gene].name,
+				children: this.ChartData['India'][gene].children,
+				value: Object.keys(this.ChartData['India'][gene].children).length,
+			})
+		}
 	},
 	mounted() {
 		this.$nextTick(()=>{
