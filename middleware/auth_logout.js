3710570c4dba8f6c8888f92191dffd3108ed94cb
Animesh_Singh <animesh.workplace@gmail.com>
2021-07-10 11:33:37 +0530

    remove the console.log

--- middleware/auth_logout.js
+++ middleware/auth_logout.js
@@ -6,14 +6,11 @@ export default async function ({ app, redirect }) {
 	let time_now_ms = new Date().getTime()
 	let time_left = expiry_time_ms - time_now_ms
 	if(store.getters.get_timeout == null) {
-		console.log('timeout is null in store')
 		if(time_left > 1000) {
-			console.log('Time is greater than 1000')
 			let time_out_function = setTimeout(function() {
 										store.dispatch('user-info-store/user_logout')
 									}, time_left);
 		} else {
-			console.log('Time is less than 1000')
 			store.dispatch('user-info-store/user_logout_server')
 			return redirect('/login')
 		}
