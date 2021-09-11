18220487b7e24fb6e8288bd0bdab155a580f900c
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-09-11 16:23:15 +0530

    mobile navbar included

--- components/header/navbar_landing.vue
+++ components/header/navbar_landing.vue
@@ -1,34 +1,69 @@
 <template>
-	<vs-navbar class="py-3 px-0 box is-floating is-sticky has-background-theme is-radiusless">
-		<template #left>
-			<div v-if="$screen.breakpoint != 'mobile'">
+	<div>
+		<vs-navbar class="py-3 px-0 box is-floating is-sticky has-background-theme is-radiusless">
+			<template #left>
+				<div v-if="$screen.breakpoint != 'mobile'">
+					<nuxt-link to="/upload">
+						<Logo class="image has-logo-size"/>
+					</nuxt-link>
+				</div>
+			</template>
+
+			<div v-show="mobile_device || $screen.breakpoint == 'mobile'">
 				<nuxt-link to="/upload">
 					<Logo class="image has-logo-size"/>
 				</nuxt-link>
 			</div>
-		</template>
 
-		<div v-show="mobile_device || $screen.breakpoint == 'mobile'">
-			<nuxt-link to="/upload">
-				<Logo class="image has-logo-size"/>
-			</nuxt-link>
+			<template #right>
+				<div v-show="$screen.breakpoint != 'mobile'">
+					<div class="button is-outlined is-inverted is-success mr-2">
+						<span>Login</span>
+					</div>
+
+					<div class="button is-inverted is-info mr-2">
+						<span>Get Started</span>
+					</div>
+				</div>
+				<div v-show="$screen.breakpoint == 'mobile'">
+					<div class="button is-square is-borderless" @click="open_dropdown_menu">
+						<svg class="icon has-fill-white">
+							<use xlink:href="@/assets/images/icons/bds.svg#dots-vert-g"></use>
+						</svg>
+					</div>
+
+
+				</div>
+			</template>
+		</vs-navbar>
+
+		<div
+			v-if="dropdown_menu"
+			class="box box-width is-floating is-sticky has-background-green is-radiusless mt-7"
+		>
+			<div class="menu has-text-centered is-inverted">
+				<ul class="menu-list">
+					<li>
+						<a>
+							<svg class="icon has-fill-white">
+								<use xlink:href="@/assets/images/icons/bds.svg#person-assign-g"></use>
+							</svg>
+							Login
+						</a>
+					</li>
+					<li>
+						<a>
+							<svg class="icon has-fill-white">
+								<use xlink:href="@/assets/images/icons/bds.svg#badge-g"></use>
+							</svg>
+							Register
+						</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 
-		<template #right>
-			<div v-if="$screen.breakpoint != 'mobile'">
-				<div class="button is-outlined is-inverted is-success mr-2">
-					<span>Login</span>
-				</div>
-
-				<div class="button is-inverted is-info mr-2">
-					<span>Get Started</span>
-				</div>
-			</div>
-			<div v-else>
-
-			</div>
-		</template>
-	</vs-navbar>
+	</div>
 </template>
 
 <script>
@@ -37,8 +72,9 @@ import Logo from '@/assets/logo/insacog_datahub_logo_white.svg?inline'
 
 export default {
 	data: () => ({
-		mobile_device: false,
 		add_shadow: false,
+		mobile_device: false,
+		dropdown_menu: false,
 		navbar_data: [
 			// { name: 'Home', link: '/' },
 			// { name: 'Upload', link: '/upload', icon: 'share-g' },
@@ -62,6 +98,9 @@ export default {
 		logout() {
 			this.$store.dispatch('user-info-store/user_logout')
 		},
+		open_dropdown_menu() {
+			this.dropdown_menu = !this.dropdown_menu
+		}
 	}
 };
 </script>
@@ -82,5 +121,12 @@ export default {
 }
 .has-background-theme {
 	background: #0a2d4d;
+}
+.mt-7 {
+	margin-top: 4.2rem!important;
+}
+.box-width {
+	width: 100%;
+	z-index: 9999;
 }
 </style>
