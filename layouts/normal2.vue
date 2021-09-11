aef5e2d3d3c1a4519d2372bb689c145f2cd32d80
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-09-11 07:51:14 +0530

    testing new layout

---
+++ layouts/normal2.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<Navbar/>
+		<Nuxt/>
+	</div>
+</template>
+
+<script>
+import Navbar from '@/components/navbar/top-nav2.vue'
+
+export default {
+	name: 'normal-layout',
+	data: () => ({
+	}),
+	components: {
+		Navbar
+	},
+	computed: {
+	},
+	beforeMount(){
+		if(this.$auth.loggedIn) {
+			console.log('This message should appear after refresh')
+			this.$store.dispatch('websocket_connect')
+			this.$store.dispatch('user-info-store/set_uploaded_metadata')
+			// this.$store.dispatch('user-info-store/set_download_link')
+		}
+	},
+};
+</script>
+
+<style scoped></style>
