dfca7a2abe3d7d7c908876d79804be5d12d09407
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-09-26 20:31:26 +0530

    updated the websocket link

--- plugins/vue-websocket.js
+++ plugins/vue-websocket.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueNativeSock from 'vue-native-websocket'
 
 export default ({ store }, inject) => {
-  Vue.use(VueNativeSock, 'ws://10.10.6.80/insacog/wsa/frontend/', {
+  Vue.use(VueNativeSock, `${process.env.WS_BASE_URL}/frontend`, {
 		store: store,
 		format: 'json',
   		connectManually: true,
