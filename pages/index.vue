58202e7cb26873a47896ec1c74880de49ffd0fb4
Animesh_Singh <animesh.workplace@gmail.com>
2021-09-26 10:21:18 +0530

    landing page added map

--- pages/index.vue
+++ pages/index.vue
@@ -90,12 +90,28 @@
 			</p>
 			<Carousel/>
 		</section>
+
+		<section class="section is-large is-relative">
+			<div class="divider2">
+				<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+					<path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+				</svg>
+			</div>
+
+			<div class="columns">
+				<div class="column">
+					<MapChart/>
+				</div>
+				<div class="column"></div>
+			</div>
+		</section>
 	</div>
 </template>
 
 <script>
 import AnimatedNumber from 'animated-number-vue'
 import PieChart from '@/components/charts/pie-chart.vue'
+import MapChart from '@/components/charts/map-chart-landing.vue'
 import Carousel from '@/components/institutes/institute-carousel.vue'
 
 export default {
@@ -106,6 +122,7 @@ export default {
 	components: {
 		Carousel,
 		PieChart,
+		MapChart,
 		AnimatedNumber,
 	},
 	methods: {
@@ -143,6 +160,24 @@ export default {
 	height: 150px;
 }
 .divider .shape-fill {
+	fill: #EAE9EB;
+}
+.divider2 {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	z-index: -1;
+	overflow: hidden;
+	line-height: 0;
+}
+.divider2 svg {
+	height: 150px;
+	display: block;
+	position: relative;
+	width: calc(160% + 1.3px);
+}
+.divider2 .shape-fill {
 	fill: #EAE9EB;
 }
 .has-background-theme {
