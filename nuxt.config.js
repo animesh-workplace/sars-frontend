62483d150ed3d56e3a3a9807defbae3f3817d62d
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-11-02 17:00:20 +0530

    updated link to social image

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,13 +34,13 @@ export default {
 			{ hid: 'og:url', name: 'og:url', property: 'og:url', content: 'https://research.nibmg.ac.in/insacog/' },
 			{ hid: 'og:title', name: 'og:title', property: 'og:title', content: 'INSACOG DataHub | National Institute of Biomedical Genomics' },
 			{ hid: 'og:description', name: 'og:description', property: 'og:description', content: 'INSACOG (Indian SARS-CoV-2 Consortium on Genomics or Indian SARS-CoV-2 Genetics Consortium) is the forum set up under the Ministry of Health and Family Welfare by the Government of India on 25 December 2020, to study and monitor genome sequencing and virus variation of circulating strains of COVID-19 in India. INSACOG works as a consortium of 10 national laboratories for genome sequencing located in different parts of India. All these 10 laboratories are required to share 5% of positive samples to INSACOG Data Hub for further research and studies. This is a national genomic sequencing database (INSACOG Data-Hub) center are appointed at National Institute of Biomedical Genomics (NIBMG)' },
-			{ hid: 'og:image', name: 'og:image', property: 'og:image', content: 'https://repository-images.githubusercontent.com/342469060/88372b5d-c9d6-4f36-b461-5810e9d634f2' },
+			{ hid: 'og:image', name: 'og:image', property: 'og:image', content: 'https://repository-images.githubusercontent.com/342469060/849ca85d-e631-4ab1-83d0-55f78fc9c616' },
 			// Twitter
 			{ hid: 'twitter:card', name: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' },
 			{ hid: 'twitter:url', name: 'twitter:url', property: 'twitter:url', content: 'https://research.nibmg.ac.in/insacog/' },
 			{ hid: 'twitter:title', name: 'twitter:title', property: 'twitter:title', content: 'INSACOG DataHub | National Institute of Biomedical Genomics' },
 			{ hid: 'twitter:description', name: 'twitter:description', property: 'twitter:description', content: 'INSACOG (Indian SARS-CoV-2 Consortium on Genomics or Indian SARS-CoV-2 Genetics Consortium) is the forum set up under the Ministry of Health and Family Welfare by the Government of India on 25 December 2020, to study and monitor genome sequencing and virus variation of circulating strains of COVID-19 in India. INSACOG works as a consortium of 10 national laboratories for genome sequencing located in different parts of India. All these 10 laboratories are required to share 5% of positive samples to INSACOG Data Hub for further research and studies. This is a national genomic sequencing database (INSACOG Data-Hub) center are appointed at National Institute of Biomedical Genomics (NIBMG)' },
-			{ hid: 'twitter:image', name: 'twitter:image', property: 'twitter:image', content: 'https://repository-images.githubusercontent.com/342469060/88372b5d-c9d6-4f36-b461-5810e9d634f2' },
+			{ hid: 'twitter:image', name: 'twitter:image', property: 'twitter:image', content: 'https://repository-images.githubusercontent.com/342469060/849ca85d-e631-4ab1-83d0-55f78fc9c616' },
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: `${process.env.ROUTER_BASE}/favicon.ico` },
