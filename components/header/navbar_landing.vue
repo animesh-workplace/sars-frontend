3006dbe55ddc566ad8448035ec41d46aebdb0ec8
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-11-02 14:27:41 +0530

    added download feature

--- components/header/navbar_landing.vue
+++ components/header/navbar_landing.vue
@@ -15,7 +15,7 @@
 				</nuxt-link>
 			</div>
 
-			<div v-if="$auth.loggedIn && !$device.isMobile" class="shift-center">
+			<div v-if="$auth.loggedIn && !$device.isMobile" :class="show_download ? 'are-you-able-to-see-this' : 'shift-center'">
 				<vs-navbar-item
 					v-for="(menu, index) in navbar_data" :key="index"
 					:id="menu.name" :active="active == menu.name" :to="menu.link"
@@ -33,7 +33,7 @@
 			</div>
 
 			<template #right>
-				<div v-show="!$device.isMobile">
+				<div class="level mb-0" v-show="!$device.isMobile">
 					<div
 						@click="activate_login" v-if="!$auth.loggedIn"
 						class="button is-fullwidth is-inverted is-success mr-2"
@@ -41,10 +41,81 @@
 						<span>Login</span>
 					</div>
 
+					<div class="field has-addons mb-0 mr-2" v-show="$auth.loggedIn && show_download" v-click-outside="handleBlur">
+						<div class="control">
+							<div class="button is-fullwidth is-success" @click="download">
+								<svg class="icon has-fill-white">
+									<use xlink:href="@/assets/images/icons/bds.svg#export-g"></use>
+								</svg>
+								<span>Download all</span>
+							</div>
+						</div>
+
+						<div class="control">
+							<div
+								:class="time_dropdown ? 'dropdown is-iconless is-right is-active' : 'dropdown is-iconless is-right'"
+							>
+								<div class="dropdown-trigger">
+									<div
+										@click="activate_time_dropdown"
+										:class="time_dropdown ? 'button has-background-success-dark' : 'button has-background-success'"
+									>
+										<svg class="icon has-fill-white">
+											<use xlink:href="@/assets/images/icons/bds.svg#arrow-down-g"></use>
+										</svg>
+									</div>
+								</div>
+								<div class="dropdown-menu">
+									<div class="dropdown-content width-adjust p-3">
+										<p class="has-text-weight-medium is-size-5 has-text-centered">
+											Limit your download to last
+										</p>
+										<div class="level my-2">
+											<input
+												type="number"
+												placeholder="Enter Period"
+												class="input is-small has-rounded-input mr-2"
+											>
+											<div class="dropdown is-hoverable is-size-6">
+												<div class="dropdown-trigger">
+													<div class="button has-rounded-input">
+														<span class="has-text-weight-normal is-size-5">
+															{{ !selected_time_period.pristine ? selected_time_period.period : 'Select Interval' }}
+														</span>
+													</div>
+												</div>
+
+												<div class="dropdown-menu width-100p">
+													<div class="dropdown-content">
+														<a
+															v-for="(time, index) in time_period"
+															:class="time.active ? 'dropdown-item is-active has-text-weight-semibold has-text-black mb-1' : 'dropdown-item mb-1'"
+															@click="select_time_period(time.name, index)"
+														>
+															{{ time.name }}
+														</a>
+													</div>
+												</div>
+											</div>
+										</div>
+
+										<div class="has-text-centered mb-2">
+											<div class="button is-light has-rounded-input">
+												<span>Download</span>
+											</div>
+										</div>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
 					<div class="button is-fullwidth is-danger mr-2" v-if="$auth.loggedIn" @click="logout">
 						<span>Logout</span>
 					</div>
 				</div>
+
 				<div v-show="$device.isMobile">
 					<div class="button is-square is-borderless" @click="open_dropdown_menu">
 						<svg class="icon has-fill-white">
@@ -97,6 +168,7 @@
 </template>
 
 <script>
+import { map } from 'lodash'
 import { mapFields } from 'vuex-map-fields'
 import Logo from '@/assets/logo/insacog_datahub_logo_white.svg?inline'
 import LoginLayout from '@/components/authentication/login-layout.vue'
@@ -106,12 +178,22 @@ export default {
 		add_shadow: false,
 		login_active: false,
 		dropdown_menu: false,
+		time_dropdown: false,
 		is_authenticated: false,
+		selected_time_period: {
+			pristine: true,
+			value: 0,
+			period: ''
+		},
+		time_period: [
+			{ name: 'Days', active: false },
+			{ name: 'Months', active: false },
+			{ name: 'Year', active: false },
+		],
 		id: Date.now() + Math.floor(Math.random()*10000 + 1),
 		navbar_data: [
 			{ name: 'Upload', link: '/upload', icon: 'share-g' },
 			{ name: 'Sequences', link: '/my_data', icon: 'timelines-g' },
-			// { name: 'Dashboard', link: '/dashboard', icon: 'segment-g' },
 		],
 	}),
 	computed: {
@@ -119,7 +201,8 @@ export default {
 			'active'
 		]),
 		...mapFields('user-info-store',[
-			'download_link'
+			'download_link',
+			'show_download'
 		])
 	},
 	components: {
@@ -146,14 +229,48 @@ export default {
 		open_dropdown_menu() {
 			this.dropdown_menu = !this.dropdown_menu
 		},
+		activate_time_dropdown() {
+			if(this.show_download) {
+				this.time_dropdown = !this.time_dropdown
+			} else {
+				this.$vs.notification({
+					sticky: true,
+					color: 'danger',
+					duration: 'none',
+					position: 'top-center',
+					title: 'Well, would you look at that!!',
+					text: "This doesn't work for you, don't try to be smart"
+				})
+			}
+		},
+		select_time_period(value, index) {
+			this.selected_time_period.pristine = false
+			this.selected_time_period.period = value
+			map(this.time_period, d=>d.active = false)
+			this.time_period[index].active = true
+		},
+		handleBlur() {
+			this.time_dropdown = false
+		},
 		async download() {
-			this.loader = this.$vs.loading()
-			await this.$store.dispatch('user-info-store/set_download_link')
-			let link = document.createElement('a')
-			link.target = '_blank'
-			link.href = this.download_link
-			link.click()
-			this.loader.close()
+			if(this.show_download) {
+				this.loader = this.$vs.loading()
+				await this.$store.dispatch('user-info-store/set_download_link')
+				let link = document.createElement('a')
+				link.target = '_blank'
+				link.href = this.download_link
+				link.click()
+				this.loader.close()
+			} else {
+				this.$vs.notification({
+					sticky: true,
+					color: 'danger',
+					duration: 'none',
+					position: 'top-center',
+					title: 'Well, would you look at that!!',
+					text: "This doesn't work for you, don't try to be smart."
+				})
+			}
 		}
 	}
 };
@@ -185,5 +302,14 @@ export default {
 }
 .shift-center {
 	margin-right: 15vw;
+}
+.width-adjust{
+	width: 500px;
+}
+.has-rounded-input {
+	border-radius: 0.25rem !important;
+}
+.width-100p {
+	width: 100%;
 }
 </style>
