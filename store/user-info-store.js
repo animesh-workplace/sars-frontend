c6ad4875c3129b199eb00d240b59a0fed458dea8
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-11-22 18:59:54 +0530

    setting download and export options on login and refresh

--- store/user-info-store.js
+++ store/user-info-store.js
@@ -5,6 +5,7 @@ import { getField, updateField } from 'vuex-map-fields'
 export const state = () => ({
 	token_expiry: '',
 	download_link: '',
+	show_export: false,
 	show_download: false,
 	uploaded_metadata: {},
 });
@@ -26,6 +27,9 @@ export const mutations = {
 	SET_SHOW_DOWNLOAD(state, payload) {
 		state.show_download = payload
 	},
+	SET_SHOW_EXPORT(state, payload) {
+		state.show_export = payload
+	},
 	updateField,
 }
 
@@ -36,22 +40,34 @@ export const actions = {
 			this.$auth.setToken('local', `JWT ${ data.token }`)
 			this.$auth.setRefreshToken('local', data.token)
 			this.$axios.setHeader('Authorization', `JWT ${ data.token }`)
-			this.$auth.setUser(data.username)
 			this.$auth.ctx.app.$axios.setHeader('Authorization', `JWT ${ data.token }`)
 			await commit('SET_TOKEN_EXPIRY', data.expires)
 			await commit('SET_SHOW_DOWNLOAD', data.download)
 			this.$auth.$storage.setCookie('token_expiry', data.expires)
-			this.$auth.$storage.setCookie('download', data.download)
+			await dispatch('get_user_status')
 			await dispatch('set_uploaded_metadata')
 			await dispatch('set_download_link')
 			await this.dispatch('websocket_connect')
-			// localStorage.setItem('token_expiry', data.expires)
 			this.$router.push('/upload')
 			Toast.open({
 				message: `Logged In Successfully`,
 				type: 'is-success',
 				position: 'is-bottom'
 			})
+		} catch(err) {
+			Toast.open({
+				message: err.response.data['message'][0] || err.response.data || err,
+				type: 'is-danger',
+				position: 'is-bottom'
+			})
+		}
+	},
+	async get_user_status({ commit, dispatch }) {
+		try {
+			const data = await this.$axios.$post('/auth/user-info/')
+			this.$auth.setUser(data.username.split('_')[1])
+			await commit('SET_SHOW_EXPORT', data.export)
+			await commit('SET_SHOW_DOWNLOAD', data.download)
 		} catch(err) {
 			Toast.open({
 				message: err.response.data['message'][0] || err.response.data || err,
@@ -84,10 +100,8 @@ export const actions = {
 		const metadata_header = await this.$axios.$post('/files/metadata-info-name/')
 		commit('SET_UPLOADED_METADATA', metadata_header)
 	},
-	async set_download_link({ commit }) {
-		let download_enabled = this.$auth.$storage.getCookie('download')
-		await commit('SET_SHOW_DOWNLOAD', download_enabled)
-		if(download_enabled) {
+	async set_download_link({ commit, state }) {
+		if(state.show_download) {
 			const link_api = await this.$axios.$post('/files/metadata-download/')
 			let link = link_api
 			commit('SET_DOWNLOAD_LINK', link)
