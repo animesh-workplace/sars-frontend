0fb178ed19ed3fd703901f1867776fb1633b796d
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-01-14 12:35:24 +0530

    added xaxis data here

--- components/charts/stacked-area-chart.vue
+++ components/charts/stacked-area-chart.vue
@@ -163,6 +163,21 @@ export default {
 				}))
 				this.options.series = s
 				this.options.legend.data = only_name
+				this.options.xAxis.data = [
+					'Jan-2021',
+					'Feb-2021',
+					'Mar-2021',
+					'Apr-2021',
+					'May-2021',
+					'Jun-2021',
+					'Jul-2021',
+					'Aug-2021',
+					'Sep-2021',
+					'Oct-2021',
+					'Nov-2021',
+					'Dec-2021',
+					'Jan-2022',
+				]
 				if(this.$device.isMobileOrTablet) {
 					this.options.grid.y = '30%'
 				}
